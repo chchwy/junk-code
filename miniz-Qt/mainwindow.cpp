9f2e946775b1f81c67e8f1d19726f9a5e0269fc4
Matt Chang <chchwy@gmail.com>
2017-12-19 23:27:20 +1100

    zzz

--- miniz-Qt/mainwindow.cpp
+++ miniz-Qt/mainwindow.cpp
@@ -25,4 +25,27 @@ void MainWindow::openFileClicked()
     qDebug() << fileName;
 
     ui->fileNameLabel->setText(fileName);
+
+    mz_zip_archive* mz = new mz_zip_archive;
+    mz_zip_zero_struct(mz);
+
+    mz_bool ok = mz_zip_reader_init_file(mz, L"D:\\新宿で20年.zip", 0);
+    printf("Open ok=%d\n", ok);
+
+    int num = mz_zip_reader_get_num_files(mz);
+    printf("num=%d\n", num);
+
+    mz_zip_archive_file_stat* stat = new mz_zip_archive_file_stat;
+    for (int i = 0; i < num; ++i)
+    {
+        ok = mz_zip_reader_file_stat(mz, i, stat);
+        printf(" item=%s\n", stat->m_filename);
+
+        if (stat->m_is_directory)
+        {
+            //CreateDirectoryA(stat->m_filename, NULL);
+        }
+        ok = mz_zip_reader_extract_to_file(mz, i, stat->m_filename, 0);
+        printf("extract ok=%d\n", ok);
+    }
 }
