ab65d29adf151cc568642661e7aa31f09c90edc7
Matt Chang <chchwy@gmail.com>
2018-04-21 15:23:40 +1000

    test

---
+++ lua-binding-cpp/hello.lua
@@ -0,0 +1,12 @@
+player = {
+    pos = {
+         X = 20,
+         Y = 30,
+    },
+    filename = "res/images/player.png",
+    HP = 20,
+-- you can also have comments
+}
+
+width = 1024
+height = 768
