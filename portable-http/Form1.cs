407564c38e3d858fa7cb5f19ac80644759946117
Matt Chang <chchwy@gmail.com>
2018-07-04 15:27:49 +1000

    portable-http

---
+++ portable-http/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace portable_http
+{
+    public partial class Form1 : Form
+    {
+        System.Threading.Thread myThread;
+        SimpleHTTPServer server;
+   
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            server = new SimpleHTTPServer("D:/Github/executive-functions/public", 3445);
+            this.FormClosed += new FormClosedEventHandler(this.Form1_Closed);
+        }
+
+        private void Form1_Closed(object sender, EventArgs e)
+        {
+            server.Stop();
+        }
+    }
+}
