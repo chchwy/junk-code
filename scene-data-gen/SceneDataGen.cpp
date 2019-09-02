fe6506d75b1e248bee91d23c4aef5efe48aecb08
Matt Chang <chchwy@gmail.com>
2019-09-02 14:32:19 +1000

    Generate level info

--- scene-data-gen/SceneDataGen.cpp
+++ scene-data-gen/SceneDataGen.cpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <map>
 #include "json.hpp"
@@ -10,6 +11,8 @@
 using json = nlohmann::json;
 
 json cameraPos;
+
+json Plans2d;
 
 json prepareCameraPos()
 {
@@ -32,17 +35,34 @@ json findCameraPosByName(std::string apartmentNumber) {
     return json::array();
 }
 
+int getLevelNumber(int apartmentNumber)
+{
+    if (apartmentNumber <= 3) return 0;
+    if (apartmentNumber <= 7) return 1;
+    if (apartmentNumber <= 12) return 2;
+    if (apartmentNumber <= 17) return 3;
+    if (apartmentNumber <= 21) return 4;
+}
+
+std::pair<int, int> getApartmentRange(int level) {
+    if (level == 0) return std::make_pair(1, 3);
+    if (level == 1) return std::make_pair(4, 7);
+    if (level == 2) return std::make_pair(8, 12);
+    if (level == 3) return std::make_pair(13, 17);
+    if (level == 4) return std::make_pair(18, 21);
+}
+
+std::string apartmentNumberStr(int i) {
+    char buffer[1024];
+    memset(buffer, 0, 1024);
+    sprintf_s(buffer, 1024, "%03d", i);
+    std::string apartmentNumber = std::string(buffer);
+    return apartmentNumber;
+}
+
 json getCameraObject(json camPosArray)
 {
     json cameraObject = json::object();
-    /*{
-        "center": [114.189, 12.785, 29.044],
-        "alpha": 45,
-        "beta": 54,
-        "distance": 30,
-        "maxDistance": 60,
-        "minDistance": 15,
-    }*/
     cameraObject["center"] = camPosArray;
     cameraObject["alpha"] = 45;
     cameraObject["beta"] = 54;
@@ -52,23 +72,13 @@ json getCameraObject(json camPosArray)
     return cameraObject;
 }
 
-json getModelsArray(std::string apartmentNumber)
+json getModelsArray(int apartment)
 {
+    std::string apartmentNumber = apartmentNumberStr(apartment);
+
     json modelsArray = json::array();
 
     json plan2d = json::object();
-    /*{
-          "name": "FloorPlan2D 019 ",
-          "path": "floorplan/FloorPlan019.glb",
-          "transform": {
-            "position": [114.189, 10, 29.044],
-            "rotation": [0, 133, 0],
-            "scale":[10 ,10, 10],
-          },
-          "nodeVisibility":{},
-          "type": "floorplan2D",
-          "pickable": false,
-      }*/
     plan2d["name"] = "2D FloorPlan " + apartmentNumber;
     plan2d["path"] = "floorplan/fp_b23_a_" + apartmentNumber + ".gltf";
     plan2d["type"] = "floorplan2D";
@@ -83,24 +93,49 @@ json getModelsArray(std::string apartmentNumber)
     plan2d["pickable"] = false;
     modelsArray.push_back(plan2d);
 
+    Plans2d.push_back(plan2d);
+
     json plan3d = json::object();
-    /*
-    {
-          "name": "FloorPlan3D 019",
-          "path": "floorplan/Apartment019d.glb",
-          "transform": {
-            "position": [0, 0, 0],
-            "rotation": [0, 0, 0],
-            "scale":[1 ,1, 1],
-          },
-          "type": "floorplan3D",
-          "pickable": false,
-    },*/
     plan3d["name"] = "3D FloorPlan " + apartmentNumber;
-    plan3d["path"] = "floorplan/building1_level4.glb";
+
+    int levelNumber = getLevelNumber(std::stoi(apartmentNumber));
+
+    std::stringstream sout;
+    sout << "floorplan/building1_level" << levelNumber << ".glb";
+    plan3d["path"] = sout.str();
     plan3d["type"] = "floorplan3D";
-    plan3d["transform"] = json::object();
+    json transform3d = json::object();
+    {
+        transform3d["position"] = json::array({ 0, 0, 0 });
+        transform3d["rotation"] = json::array({ 0, 0, 0 });
+        transform3d["scale"] = json::array({ 1, 1, 1 });
+    }
+    plan3d["transform"] = transform3d;
     plan3d["nodeVisibility"] = json::object();
+    {
+        std::pair<int, int> range = getApartmentRange(levelNumber);
+        int min = range.first;
+        int max = range.second;
+        for (int i = min; i <= max; ++i) {
+            std::stringstream sout;
+            sout << "apartment_geometry/Block2_3/Building_1_vqxyyp/level_" << levelNumber
+                << "/apartment" << apartmentNumberStr(i);
+
+            std::string nodePath = sout.str();
+            plan3d["nodeVisibility"][nodePath] = bool(i == apartment);
+
+            /*
+            "apartment_geometry/Block2_3/Building_1_vqxyyp/level_4/apartment018": false,
+            "apartment_geometry/Block2_3/Building_1_vqxyyp/level_4/apartment019" : true,
+            "apartment_geometry/Block2_3/Building_1_vqxyyp/level_4/apartment020" : false,
+            "apartment_geometry/Block2_3/Building_1_vqxyyp/level_4/apartment021" : false,
+            "apartment_geometry/Block2_3/Building_1_vqxyyp/level_4/Corridor_nvabyz" : false,
+            */
+        }
+        std::stringstream sout;
+        sout << "apartment_geometry/Block2_3/Building_1_vqxyyp/level_" << levelNumber << "/Corridor";
+        plan3d["nodeVisibility"][sout.str()] = false;
+    }
     plan3d["pickable"] = false;
     modelsArray.push_back(plan3d);
 
@@ -112,15 +147,15 @@ int main()
     std::cout << "Hello World!\n";
 
     cameraPos = prepareCameraPos();
+    Plans2d = json::array();
 
     json j = json::array();
     
     for (int i = 1; i <= 21; ++i)
     {
-        char buffer[1024];
-        sprintf_s(buffer, 1024, "%03d", i);
-        std::string apartmentNumber = std::string(buffer);
+        std::string apartmentNumber = apartmentNumberStr(i);
         
+        char buffer[1024];
         sprintf_s(buffer, 1024, "00000000-0000-0000-0000-%012d", i);
         std::string sceneGUID = std::string(buffer);
 
@@ -129,7 +164,7 @@ int main()
         scene["sceneId"] = sceneGUID;
         scene["type"] = "unit";
         scene["camera"] = getCameraObject(findCameraPosByName(apartmentNumber));
-        scene["models"] = getModelsArray(apartmentNumber);
+        scene["models"] = getModelsArray(i);
         scene["components"] = json::array({ json::object({ {"name", "FloorPlanSwitch"} }) });
         
         j.push_back(scene);
@@ -142,5 +177,13 @@ int main()
     fout << str;
     fout.flush();
     fout.close();
+
+    std::string plans2d_string = Plans2d.dump(2);
+    std::cout << plans2d_string;
+
+    std::ofstream fout2("floorplan2.json", std::ofstream::out);
+    fout2 << plans2d_string;
+    fout2.flush();
+    fout2.close();
     return 0;
 }
